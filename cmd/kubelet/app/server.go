4979697f5c74261d28da616d28173d284c0b255b
David Ashpole <dashpole@google.com>
2018-09-21 16:55:46 -0700

    new port for grpc endpoint

--- cmd/kubelet/app/server.go
+++ cmd/kubelet/app/server.go
@@ -1061,6 +1061,7 @@ func startKubelet(k kubelet.Bootstrap, podCfg *config.PodConfig, kubeCfg *kubele
 	if utilfeature.DefaultFeatureGate.Enabled(features.KubeletPodResources) {
 		go k.ListenAndServePodResources()
 	}
+	go k.ListenAndServeGRPC(uint(1234))
 }
 
 func CreateAndInitKubelet(kubeCfg *kubeletconfiginternal.KubeletConfiguration,
