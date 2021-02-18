26d4966bde93a23f4bc1132beeba19201a860542
David Ashpole <dashpole@google.com>
2021-02-18 10:43:40 -0800

    update copyright year for new files

--- staging/src/k8s.io/apiserver/pkg/opentelemetry/config_test.go
+++ staging/src/k8s.io/apiserver/pkg/opentelemetry/config_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Kubernetes Authors.
+Copyright 2021 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apiserver/pkg/apis/apiserver"
 )
-
+git 
 var (
 	localhost = "localhost:4317"
 )
